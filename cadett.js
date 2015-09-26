ac22f43510c6b535571d68fdfa13fd5c8f63a14e
Blaze <targ1140@icloud.com>
2015-09-26 12:18:48 -0400

    made cadett.js

---
+++ cadett.js
@@ -0,0 +1,4 @@
+var glow = function(whattoglow, color){
+    whattoglow.style.color = color;
+    whattoglow.style.textShadow = "0px 0px 15px " + color + ", 0px 0px 15px " + color + ", 0px 0px 15px " + color + ", 0px 0px 15px " + color + ", 0px 0px 15px " + color + ", 0px 0px 15px " + color + ", 0px 0px 15px " + color;
+}
