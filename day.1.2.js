68eca53e84c7ff9e05fad3b522af402f4a09ed58
Cadett <targ1140@icloud.com>
2015-10-09 13:41:03 -0400

    Update and rename day.1.1.js to day.1.2.js

--- day.1.2.js
+++ day.1.2.js
@@ -5,6 +5,10 @@ function checkTime(i) {
     return i;
 }
 var q = new Date();
+/** 
+ * Thanks to KCF for fixing some major bugs.
+ * Website: www.kingcodefish.com
+ */
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var dotw = dayNames[q.getDay()];
