60240b3b66c8e429a38e99e4f076314a39206c81
Cadett <targ1140@icloud.com>
2015-10-09 13:42:20 -0400

    And now the newest version.

--- day.1.3.js
+++ day.1.3.js
@@ -9,11 +9,13 @@ var q = new Date();
  * Thanks to KCF for fixing some major bugs.
  * Website: www.kingcodefish.com
  */
+ var π = {};
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var dotw = dayNames[q.getDay()];
-var day = q.getDate();
-var month = monthNames[q.getMonth()];
-var year = q.getFullYear();
-var hour = q.getHours();
-var minutes = checkTime(q.getMinutes());
+π.dotw = dayNames[q.getDay()];
+π.day = q.getDate();
+π.month = monthNames[q.getMonth()];
+π.year = q.getFullYear();
+π.hour = q.getHours();
+π.minutes = checkTime(q.getMinutes());
+π.seconds = checkTime(q.getSeconds());
